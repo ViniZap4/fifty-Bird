062fee32efb0eb555395f6e76dfd3908acff5f54
Vinicius Zapalá dos Santos <40218275+ViniZap4@users.noreply.github.com>
2021-10-12 22:36:45 -0300

    update brackground scrolling

---
+++ main.lua
@@ -0,0 +1,96 @@
+-- import libs
+
+-- https://github.com/vrld/hump/blob/master/class.lua
+Class = require "libs/class"
+
+-- https://github.com/Ulydev/push
+push = require "libs/push"
+
+-- import components
+require 'components/font'
+require 'components/audio'
+require "components/images"
+
+
+-- import images and respective scroll variables
+
+local background = Img:new("background.png")
+local backgroundScroll = 0
+
+local ground = Img:new("ground.png")
+local groundScroll = 0
+
+-- scrolling variables
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+-- Set screen dimensions
+
+WINDOW_WIDTH = 1280
+WIDOW_HEIGHT = 720
+
+-- virtual screen 
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+
+
+function love.resize(w,h)
+  push:resize(w, h)
+end
+
+function love.load()
+
+  love.window.setTitle('Fifty Bird')
+
+  -- using nearest-neighbor filter for prevent to blur
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
+  -- seed the RNG
+  math.randomseed(os.time())
+
+  Font:set(flappyFont)
+
+  -- init music 
+  sounds['music']:setLooping(true)
+  sounds['music']:play()
+
+
+  push:setupScreen(
+    VIRTUAL_WIDTH, VIRTUAL_HEIGHT,
+    WINDOW_WIDTH, WIDOW_HEIGHT,
+    {
+      fullscreen= false,
+      resizable = true,
+      vsync=true
+    }
+  )
+
+
+end
+
+function love.keypressed(key)
+  
+  if key == "escape" then 
+    love.event.quit()
+  end
+
+end
+
+function love.update(dt)
+
+  backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+  groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+  
+end
+
+function love.draw()
+  push:start()
+  love.graphics.draw(background, -backgroundScroll, 0)
+  love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+
+  push:finish()
+end
